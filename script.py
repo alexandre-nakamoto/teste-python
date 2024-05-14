28054ece5d51b67d6d7b351af39f1a3d4d29059f
alexandre-nakamoto <alexandre.nakamoto@gmail.com>
2024-05-14 20:48:55 -0300

    atualizar python2

--- script.py
+++ script.py
@@ -1,1 +1,10 @@
-print("ola mundo")
+# -*- coding: utf-8 -*-
+print("olá mundo")
+print("outra mensagem")
+print("mensagem 3")
+#teste comentário
+"""
+comentario de muitas 
+linhas
+"""
+print ("mensagem")
